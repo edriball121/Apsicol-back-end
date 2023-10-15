b49d750ad36d142ccb9e42808c531329720c3c1b
edriball121 <53952759+edriball121@users.noreply.github.com>
2023-10-15 11:48:10 -0500

    Se ajusta el servicio y el schema de trabajo
    Se ajusta el servicio y el schema de trabajo

--- schemas/job.schema.js
+++ schemas/job.schema.js
@@ -4,16 +4,16 @@ const Joi = require("joi");
 //crear schema para cada uno de los campos
 const emp_codigo = Joi.number();
 const emp_nombre = Joi.string().min(2).max(45).regex(/^[a-zA-Z,.;:" ]+$/);
-const emp_termnos_y_condiciones = Joi.string().min(2).max(45).regex(/^[a-zA-Z,.;:ñÑáéíóúÁÉÍÓÚ\s\-" ]+$/);
+const emp_terminos_y_condiciones = Joi.string().min(2).max(1000).regex(/^[a-zA-Z,.;:ñÑáéíóúÁÉÍÓÚ\s\-" ]+$/);
 const emp_fecha_creacion = Joi.string().regex(/^[0-9-]+$/);
 const emp_descripcion = Joi.string().min(2).max(1000).regex(/^[a-zA-Z,.;:ñÑáéíóúÁÉÍÓÚ\s\-" ]+$/);
 
 
 //Regla para crear trabajo
 const createJobSchema = Joi.object({
-  emp_codigo: emp_codigo.required(),
+  emp_codigo: emp_codigo,
   emp_nombre: emp_nombre.required(),
-  emp_termnos_y_condiciones: emp_termnos_y_condiciones.required(),
+  emp_terminos_y_condiciones: emp_terminos_y_condiciones.required(),
   emp_fecha_creacion: emp_fecha_creacion.required(),
   emp_descripcion: emp_descripcion.required(),
 });
@@ -22,7 +22,7 @@ const createJobSchema = Joi.object({
 const updateJobSchema = Joi.object({
   emp_codigo: emp_codigo,
   emp_nombre: emp_nombre,
-  emp_termnos_y_condiciones: emp_termnos_y_condiciones,
+  emp_terminos_y_condiciones: emp_terminos_y_condiciones,
   emp_fecha_creacion: emp_fecha_creacion,
   emp_descripcion: emp_descripcion,
 });
