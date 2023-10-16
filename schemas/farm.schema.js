d2ceb5827ce0268ef5e65ebe477643776ad57b4f
edriball121 <53952759+edriball121@users.noreply.github.com>
2023-10-15 21:27:53 -0500

    Se ajusta el schema de la finca
    Se ajusta el schema de la finca

--- schemas/farm.schema.js
+++ schemas/farm.schema.js
@@ -6,14 +6,14 @@ const fin_codigo = Joi.number();
 const fin_nombre = Joi.string().min(2).max(45).regex(/^[a-zA-Z ]+$/);
 const fin_tamanno = Joi.number();
 const fin_direccion = Joi.string().min(5).max(45);
-const fin_foto = Joi.string().min(6).max(120);
+const fin_foto = Joi.string();
 const fin_productos = Joi.string().min(10).max(150).regex(/^[a-zA-Z,.;:ñÑáéíóúÁÉÍÓÚ\s\-" ]+$/);
 const fin_telefono = Joi.string().max(10).regex(/^[0-9-]+$/);
 const fin_descripcion = Joi.string().min(5).max(1000);
 
 //Regla para crear granjero
 const createFarmSchema = Joi.object({
-  fin_codigo: fin_codigo.required(),
+  fin_codigo: fin_codigo,
   fin_nombre: fin_nombre.required(),
   fin_tamanno: fin_tamanno.required(),
   fin_direccion: fin_direccion.required(),
