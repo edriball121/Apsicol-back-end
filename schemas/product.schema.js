d2f399c00acc4a85ea8f852b14438f74f181aab7
edriball121 <53952759+edriball121@users.noreply.github.com>
2023-10-16 18:30:54 -0500

    Se ajusta el schema de productos
    Se ajusta el schema de productos

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -5,15 +5,15 @@ const Joi = require("joi");
 const pro_codigo = Joi.number();
 const pro_nombre = Joi.string().min(2).max(45).regex(/^[a-zA-Z,.;:ñÑáéíóúÁÉÍÓÚ\s\-" ]+$/);
 const pro_descripcion = Joi.string().min(2).max(1000).regex(/^[a-zA-Z,.;:ñÑáéíóúÁÉÍÓÚ\s\-" ]+$/);
-const pro_precio = Joi.number();
+const pro_precio = Joi.string().max(45);
 const pro_estado = Joi.string().max(45);
-const pro_cantidad = Joi.number();
-const pro_terminos_y_condiciones = Joi.number();
-const pro_foto = Joi.string().max(120);
+const pro_cantidad = Joi.string().max(45);
+const pro_terminos_y_condiciones = Joi.string().max(1000);
+const pro_foto = Joi.string();
 
 //Regla para crear producto
 const createProductSchema = Joi.object({
-  pro_codigo: pro_codigo.required(),
+  pro_codigo: pro_codigo,
   pro_nombre: pro_nombre.required(),
   pro_descripcion: pro_descripcion.required(),
   pro_precio: pro_precio.required(),
